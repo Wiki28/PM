8e8fe84953e0cf002b5084ab5bd752448197db28
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-03-20 00:34:29 +0800

    Update __init__.py

--- PrimeMega/modules/sql/__init__.py
+++ PrimeMega/modules/sql/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from PrimeMega import DB_URL, LOGGER
+from PrimeMega import DATABASE_URL, LOGGER
 
 
 def start() -> scoped_session:
